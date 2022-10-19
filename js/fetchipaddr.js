2f820aaf22bdb4db5921c19b08a524a14a473a9e
ERLMKea <erlm@kea.dk>
2022-10-19 14:22:38 +0200

    fetch ipaddr

--- js/fetchipaddr.js
+++ js/fetchipaddr.js
@@ -1,6 +1,8 @@
+const out = (any) => console.log(any)
 out("im in fetch ipaddr")
 
 const urlIpAddr = "https://api.ipify.org/?format=json";
+
 const lblIpAddr = document.getElementById("lblIpAddress");
 
 
@@ -11,11 +13,13 @@ function fetchAnyUrl(url) {
 
 async function actionFetchIpAddr(btn) {
     out("fetch ipaddr")
-    let ipaddr = await fetchCameras(urlIpAddr);
+    let ipaddr = await fetchAnyUrl(urlIpAddr);
     out(ipaddr); //students is an array
-    lblIpAddr.textContent = ipaddr;
+    out(ipaddr.ip);
+    lblIpAddr.textContent = ipaddr.ip;
 }
 
-const pbFetchIpAddr = document.getElementById("pbFetchIpAddress");
+const pbFetchIpAddr = document.getElementById("pbFetchIpAddress")
+out(pbFetchIpAddr)
 pbFetchIpAddr.addEventListener('click', actionFetchIpAddr)
 
